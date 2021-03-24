66113781ae0ddb8e89882bc28df84e5f6726985d
Jerry-G <jerrysgithubonly@gmail.com>
2021-03-23 23:20:59 -0500

    Add better comments

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
+// struct for parsing the json palette
 #[derive(Serialize, Deserialize)]
 struct PaletteJSON {
     colors: Vec<String>,
@@ -27,9 +28,13 @@ fn dist(c1: &image::Rgb<u8>, c2: &image::Rgb<u8>) -> f64 {
 
 // Take in a string that has a hex rgb value and return an Rgb
 fn hex_to_rgb(str: &str) -> Result<Rgb<u8>, String> {
+    // expect the color to be exactly 6 chars in length
+    // a color with the hash symbol ( #FFFFFF ) will not be accepted
     if str.len() != 6 {
         return Err(format!("unable to interpret {:?} as color", &str));
     }
+
+    // inelegant and not very fast way to turn color string into 3 u8s
     let mut r = "".to_owned();
     let mut g = "".to_owned();
     let mut b = "".to_owned();
@@ -60,10 +65,13 @@ fn hex_to_rgb(str: &str) -> Result<Rgb<u8>, String> {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    // expect exactly 4 args
     if args.len() != 4 {
         println!("Usage: paletter palette.json input.png output.png");
         std::process::exit(0);
     }
+
+    // images
     let og = image::open(Path::new(&args[2])).unwrap();
     let mut img: RgbImage = ImageBuffer::new(og.dimensions().0, og.dimensions().1);
 
@@ -83,11 +91,13 @@ fn main() {
         Ok(_) => (),
     }
 
+    // parse the palette json
     let json: PaletteJSON = match serde_json::from_str(&data) {
         Err(why) => panic!("couldn't understand {} file: {}", display, why),
         Ok(val) => val,
     };
 
+    // convert the parsed palette into native Rgb types
     let mut palette: Vec<Rgb<u8>> = Vec::new();
     for color in json.colors.iter() {
         let c: Rgb<u8> = match hex_to_rgb(color) {
@@ -97,6 +107,8 @@ fn main() {
         palette.push(c);
     }
 
+    // for each pixel calculate the dist to each color in the palette,
+    // inefficient, but not too slow in practice, could be improved
     for (x, y, pixel) in img.enumerate_pixels_mut() {
         let mut tmp_color = palette[0];
         let ogp = og.get_pixel(x, y).to_rgb();
@@ -111,5 +123,6 @@ fn main() {
         *pixel = tmp_color;
     }
 
+    // this fails if you try to write to a dir that does not exist
     img.save(Path::new(&args[3])).unwrap();
 }
